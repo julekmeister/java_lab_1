175f73edead31e80c1a77c3a3a443d20a9bf4300
julekmeister <julekmeister@gmail.com>
2021-10-02 19:24:29 +0200

    adding cwiczenie1

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,18 @@ public class Main {
         *   dane mają być wprowadzane z klawiatury w konsoli
         *   b) wyświetlić dane za pomocą println i printf
         */
+            Scanner sc=new Scanner(System.in);
 
+            System.out.print("Imię: ");
+            String name=sc.next();
+            System.out.print("Nazwisko: ");
+            String lname=sc.next();
+            System.out.print("Wiek: ");
+            String age=sc.next();
+            System.out.print("Nr indeksu: ");
+            String ind=sc.next();
+
+            System.out.printf("%s"+name+ "%s"+lname+"%s"+age+"%s"+ind    ,"\tImię: ","\tNazwisko: ","\tWiek: ","\tNr Indexu: ");
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
