6636aa9ba517e0078e2eea099591666a4532cb00
julekmeister <julekmeister@gmail.com>
2021-10-02 18:45:14 +0200

    adding cwiczenie1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
         System.out.println("Hello world!");
+        System.out.printf("Hello %s world!", "another");
 
     }
 }
